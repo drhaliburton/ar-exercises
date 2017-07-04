dd4336ec802d7bf1aed343454798035c042809fa
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-04 21:25:15 +0000

    Completed exercise 3

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create
+burnaby.name = "Burnaby"
+burnaby.annual_revenue = 300000
+burnaby.womens_apparel = true
+burnaby.mens_apparel = true
+
+richmond = Store.create
+richmond.name = "Richmond"
+richmond.annual_revenue = 1260000
+richmond.womens_apparel = true
+richmond.mens_apparel = false
+
+gastown = Store.create
+gastown.name = "Gastown"
+gastown.annual_revenue = 190000
+gastown.womens_apparel = false
+gastown.mens_apparel = true
+
+puts Store.count()
+
+puts gastown.name
