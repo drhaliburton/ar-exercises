875488822b6792d33436af03da279656464ee87a
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-05 00:01:01 +0000

    Exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,19 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# 1
+@total = Store.sum(:annual_revenue)
+
+puts @total
+
+# 2
+@average = Store.average(:annual_revenue)
+
+puts @average
+
+# 3
+
+@numOfBigBoys = Store.where("annual_revenue > ?", 1000000).count
+
+puts @numOfBigBoys
